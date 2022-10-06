1af2b3d22b8eec25963d0adab7d02371d2f66ab8
itsukicode <t.itsuki928@gmail.com>
2022-10-06 14:37:25 +0900

    feat: UI作成

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.example.artspace
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,13 +45,70 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ArtSpaceScreen() {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 50.dp, start = 30.dp, end = 30.dp, bottom = 30.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-
+        Image(
+            painter = painterResource(id = R.drawable.art1),
+            contentDescription = null,
+            modifier = Modifier
+                .border(
+                    BorderStroke(2.dp, Color(105, 205, 216)),
+                    shape = RoundedCornerShape(4.dp),
+                )
+                .padding(30.dp)
+                .fillMaxWidth()
+                .weight(1f)
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        Card(
+            elevation = 3.dp,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier.padding(10.dp)
+            ) {
+                Text(
+                    text = "Artwork Title", fontSize = 30.sp
+                )
+                Row {
+                    Text(text = "Artwork Artist", fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.size(10.dp))
+                    Text(text = "(Year)")
+                }
+            }
+        }
+        Spacer(modifier = Modifier.size(30.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = "Previous",
+                    modifier = Modifier.padding(vertical = 5.dp)
+                )
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = "Next",
+                    modifier = Modifier.padding(vertical = 5.dp)
+                )
+            }
+        }
     }
-  
 }
 
 
